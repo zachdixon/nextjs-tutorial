66b56b437f29e5419b8b027d91df822c95633aa3
Zachary Dixon <zdixon@redventures.com>
2026-01-09 03:11:12 -0600

    fix: delete invoice action

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -28,7 +28,12 @@ export function UpdateInvoice({ id }: { id: string }) {
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
-    <form action={deleteInvoiceWithId}>
+    <form
+      action={async () => {
+        "use server";
+        await deleteInvoiceWithId();
+      }}
+    >
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
