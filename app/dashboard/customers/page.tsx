b1a22f631b2cb70c27f97278930e4b909dee4253
Zachary Dixon <zdixon@redventures.com>
2026-01-09 03:21:18 -0600

    feat: add customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,33 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
+import CustomersTable from "@/app/ui/customers/table";
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable query={query} />
+      </Suspense>
+    </div>
+  );
 }
